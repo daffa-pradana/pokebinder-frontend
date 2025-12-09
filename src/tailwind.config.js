7afc3f4a57f49aa4cde21a208e19dc3e5d8c0bcc
Daffa Pradana <daffa@happy5.co>
2025-12-09 19:11:35 +0700

    adjust config for tailwind fonts

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,23 +1,39 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        card: "700",
+      },
       colors: {
-        'red-light': '#FF5350',
-        'red-bright': '#B40001',
-        'red-medium': '#C20001',
-        'bg-light': '#F6F8FC',
-        'grey-100': '#EFF1F7',
-        'grey-300': '#B2B2C3',
-        'grey-500': '#7E8E9C',
-        'navy-light': '#183352',
-        'navy-medium': '#2B2D42',
-        'navy-dark': '#0E1A46',
+        pokebinder: {
+          red: {
+            light: "#FF5350",
+            DEFAULT: "#C20001",
+            dark: "#B40001",
+          },
+          gray: {
+            light: "#F6F8FC",
+            medium: "#EFF1F7",
+            dark: "#B2B2C3",
+            darker: "#7E8E9C",
+          },
+          navy: {
+            light: "#183352",
+            medium: "#2B2D42",
+            dark: "#0E1A46",
+          },
+          white: "#FFFFFF",
+        },
       },
     },
   },
   plugins: [],
-}
+};
