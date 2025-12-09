6f49f982cb28141b1cfb7b1d9bfbb6835cf40aec
Daffa Pradana <daffa@happy5.co>
2025-12-09 19:08:48 +0700

    init tailwind

---
+++ src/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'red-light': '#FF5350',
+        'red-bright': '#B40001',
+        'red-medium': '#C20001',
+        'bg-light': '#F6F8FC',
+        'grey-100': '#EFF1F7',
+        'grey-300': '#B2B2C3',
+        'grey-500': '#7E8E9C',
+        'navy-light': '#183352',
+        'navy-medium': '#2B2D42',
+        'navy-dark': '#0E1A46',
+      },
+    },
+  },
+  plugins: [],
+}
