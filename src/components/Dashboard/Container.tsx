c026039af0c282099b8600ff87f5f5c9457d1660
Daffa Pradana <daffa@happy5.co>
2026-01-10 12:46:52 +0700

    add some layering background

--- src/components/Dashboard/Container.tsx
+++ src/components/Dashboard/Container.tsx
@@ -1,7 +1,20 @@
+import greyPokeball from '@/assets/images/grey-pokeball-img.svg';
+
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-[#F6F8FC]-50 text-gray-800 font-sans">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+    <div className="relative min-h-screen bg-[#F6F8FC]-50 text-gray-800 font-sans">
+      <img
+        src={greyPokeball}
+        className="
+          absolute
+          -top-10
+          -left-20
+          w-[420px]
+          z-0
+          pointer-events-none
+        "
+      />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 relative z-10">
         {children}
       </div>
     </div>
